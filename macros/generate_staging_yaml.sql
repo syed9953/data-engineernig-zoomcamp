0ee1b63c0b75e9212830b18cb3ae3964d1520401
Ali Ahmed <doclia2033@gmail.com>
2026-06-13 15:49:59 +0000

    Updated Models

---
+++ macros/generate_staging_yaml.sql
@@ -0,0 +1,6 @@
+{% macro generate_staging_yaml() %}
+    {% set models_to_generate = codegen.get_models(directory='core') %}
+    {{ codegen.generate_model_yaml(
+        model_names = models_to_generate
+    ) }}
+{% endmacro %}
